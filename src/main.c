b6882bf52c2484d0452389a0778b7ac10a107b52
Bruno BELANYI <bruno@belanyi.fr>
2020-11-10 18:23:14 +0100

    replace dummy output with actual mandelbrot

--- src/main.c
+++ src/main.c
@@ -3,19 +3,9 @@
 #include <stdlib.h>
 
 #include "image.h"
+#include "mandelbrot.h"
 #include "options.h"
 #include "ppm.h"
-
-static void fill_image(struct image *image) {
-    for (size_t i = 0; i < image->h; ++i) {
-        for (size_t j = 0; j < image->w; ++j) {
-            struct pixel *p = &image->buf[to_index(i, j, image)];
-            p->r = 255 * i / image->h;
-            p->g = 255 * j / image->w;
-            p->g = 255 * (i + j) / (image->h + image->w);
-        }
-    }
-}
 
 int main(int argc, char *argv[]) {
     struct options opt = parse_options(&argc, &argv);
@@ -24,7 +14,7 @@ int main(int argc, char *argv[]) {
     if (!image)
         err(EXIT_FAILURE, "could not allocate image");
 
-    fill_image(image);
+    mandelbrot(image, 100);
     print_ppm(image, opt.output);
 
     fclose(opt.output);
